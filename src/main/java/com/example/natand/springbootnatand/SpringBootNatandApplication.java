422b7751811a0e09ce468bb965fbe7527e3a7776
depauna <natan.depauw@flowfactor.be>
2019-04-17 09:52:14 +0200

    Init

---
+++ src/main/java/com/example/natand/springbootnatand/SpringBootNatandApplication.java
@@ -0,0 +1,13 @@
+package com.example.natand.springbootnatand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNatandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNatandApplication.class, args);
+	}
+
+}
