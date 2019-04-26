fdc8ead880f9f58dab4e057ae01669807f32a954
depauna <natan.depauw@flowfactor.be>
2019-04-26 13:29:01 +0200

    branch for mysql

--- src/main/java/com/example/natand/springbootnatand/SpringBootNatandApplication.java
+++ src/main/java/com/example/natand/springbootnatand/SpringBootNatandApplication.java
@@ -19,37 +19,37 @@ public class SpringBootNatandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNatandApplication.class, args);
-//		ApplicationContext applicationContext = SpringApplication.run(SpringBootNatandApplication.class, args);
-//		EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
-//
-//		Employee employee0 = new Employee();
-//		employee0.setEmpId(1);
-//		employee0.setName("Natan");
-//		employee0.setDesignation("DevOps Engineer");
-//		employee0.setSalary(3000);
-//
-//		Employee employee1 = new Employee();
-//		employee1.setEmpId(2);
-//		employee1.setName("Charlotte");
-//		employee1.setDesignation("Developer");
-//		employee1.setSalary(2500);
-//
-//		Employee employee2 = new Employee();
-//		employee2.setEmpId(3);
-//		employee2.setName("Harry");
-//		employee2.setDesignation("Sorcerer");
-//		employee2.setSalary(9000);
-//
-//		employeeService.insertEmployee(employee0);
-//
-//		List<Employee> employees = new ArrayList<>();
-//		employees.add(employee1);
-//		employees.add(employee2);
-//		employeeService.insertEmployees(employees);
-//
-//		employeeService.getAllEmployees();
-//
-//		employeeService.getEmployeeById(employee1.getEmpId());
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootNatandApplication.class, args);
+		EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
+
+		Employee employee0 = new Employee();
+		employee0.setEmpId(1);
+		employee0.setName("Natan");
+		employee0.setDesignation("DevOps Engineer");
+		employee0.setSalary(3000);
+
+		Employee employee1 = new Employee();
+		employee1.setEmpId(2);
+		employee1.setName("Charlotte");
+		employee1.setDesignation("Developer");
+		employee1.setSalary(2500);
+
+		Employee employee2 = new Employee();
+		employee2.setEmpId(3);
+		employee2.setName("Harry");
+		employee2.setDesignation("Sorcerer");
+		employee2.setSalary(9000);
+
+		employeeService.insertEmployee(employee0);
+
+		List<Employee> employees = new ArrayList<>();
+		employees.add(employee1);
+		employees.add(employee2);
+		employeeService.insertEmployees(employees);
+
+		employeeService.getAllEmployees();
+
+		employeeService.getEmployeeById(employee1.getEmpId());
 	}
 
 }
